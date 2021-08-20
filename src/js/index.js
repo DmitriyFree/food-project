14907a2ec86fd0337f28fe94b75e7432401fa2c7
Dmitriy <dmitriyolv@gmail.com>
2021-08-20 23:58:09 +0300

    adding timer

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,4 @@
 import '../scss/style.scss';
+import timer from './modules/timer';
+
+timer('promotion__timer', '2021-08-31');
---
+++ src/js/modules/timer.js
@@ -0,0 +1,57 @@
+function timer(selector, endtime) {
+
+  function calcTime(endtime) {
+
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+          days = Math.round(t / (1000 * 60 * 60 * 24)),
+          hours = Math.round((t / (1000 * 60 * 60)) % 24),
+          minutes = Math.round((t / (1000 * 60)) % 60),
+          seconds = Math.round((t / (1000)) % 60);
+
+    return {
+      total = t,
+      days = days,
+      hours = hours,
+      minutes = minutes,
+      seconds = seconds
+    }
+
+  }
+
+  function convertZero (num) {
+    if (num >= 0 && num < 10) {
+       return `0${num}`;
+    }
+    else return num;
+  }
+
+  function renderTime(selector) {
+
+    const timer = document.querySelector(selector),
+    days = timer.querySelector('#days'),
+    hours = timer.querySelector('#hours'),
+    minutes = timer.querySelector('#minutes'),
+    seconds = timer.querySelector('#seconds'),
+    timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+
+      const t = getTimeRemaining(endtime);
+      days.textContent = convertZero(t.days);
+      hours.textContent = convertZero(t.hours);
+      minutes.textContent = convertZero(t.minutes);
+      seconds.textContent = convertZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  renderTime(selector);
+
+}
+
+export default timer;
