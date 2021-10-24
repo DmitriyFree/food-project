dfe9a7cca80b967e8ba442adb3a438363b130b4c
Dmitriy <dmitriyolv@gmail.com>
2021-10-24 20:59:25 +0300

    edit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./server.json');
 const middlewares = jsonServer.defaults({
-  static: './build'
+  static: './'
 });
 const PORT = process.env.PORT || 8000;
 server.use(middlewares);
